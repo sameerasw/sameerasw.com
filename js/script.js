576b6fc8536b531eba7197381f4c296a47c26eb9
sameerasw <sameera.whoami@gmail.com>
2024-02-07 22:19:05 +0530

    New welcome animationsgit add .

--- js/script.js
+++ js/script.js
@@ -20,18 +20,36 @@ window.addEventListener('scroll', function(){
     } else{
         logo.style.height = '8em';
     }
-    //hides the nav bar when the user scrolls down and show it when the user scrolls up
-    let lastScrollTop = 0;
-    window.addEventListener('scroll', function(){
-        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-        if(currentScroll > lastScrollTop){
-            nav.style.bottom = "-20em";
-            totop.style.bottom = "1em";
-        } else{
-            nav.style.bottom = "1em";
-            totop.style.bottom = "-20em";
-        }
-        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-    }, false);
 }
 );
+
+//hides the nav bar when the user scrolls down and show it when the user scrolls up
+let lastScrollTop = 0;
+window.addEventListener('scroll', function(){
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if(currentScroll > lastScrollTop){
+        nav.style.bottom = "-20em";
+        totop.style.bottom = "1em";
+    } else{
+        nav.style.bottom = "1em";
+        totop.style.bottom = "-20em";
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+}, false);
+
+//animate each item one by one when the page is loaded
+let items = document.querySelectorAll('.item');
+let i = 0;
+
+function animate(){
+    if(i < items.length){
+        items[i].style.opacity = 1;
+        items[i].style.transform = "translateY(0)";
+        i++;
+        setTimeout(animate, 150);
+    }
+}
+
+window.onload = function(){
+    animate();
+}
