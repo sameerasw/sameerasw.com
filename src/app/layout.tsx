52bbf4550a72554a3f3272aec8a3192681a27359
sameerasw <sameerasw.com@gmail.com>
2026-04-13 00:34:02 +0530

    fix: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,11 +33,6 @@ export const metadata: Metadata = {
     images:
       "https://raw.githubusercontent.com/sameerasw/sameerasw.com/main/assets/img/web-preview.png",
   },
-  icons: {
-    icon: "/assets/img/logo-mini.png",
-    shortcut: "/assets/img/logo-mini.png",
-    apple: "/assets/img/logo-mini.png",
-  },
 };
 
 export default function RootLayout({
