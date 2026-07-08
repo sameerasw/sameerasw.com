2ad7b522c255364b932b996bd5e04282b686dae9
sameerasw <sameerasw.com@gmail.com>
2026-07-08 22:56:07 +0530

    fix: update form submission endpoint to /__forms.html

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -46,7 +46,7 @@ export default function HomeClient({
     const formData = new FormData(form);
 
     try {
-      const response = await fetch("/", {
+      const response = await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(formData as any).toString(),
